df0828d2d4ebb9d84b6969785e7d17676048cebe
crh82 <103348212+crh82@users.noreply.github.com>
2022-05-10 17:53:43 +1000

    Assets to be added

---
+++ Assets/Scripts/TestScripts/TestStateUIController.cs
@@ -0,0 +1,34 @@
+using System;
+using Michsky.UI.ModernUIPack;
+using UnityEngine;
+
+namespace TestScripts
+{
+    public class TestStateUIController : MonoBehaviour
+    {
+        public ModalWindowManager wind;
+        public GameObject stateObject;
+        private State _state;
+
+        public WindowDragger window;
+
+        private void Awake()
+        {
+            _state = gameObject.GetComponentInParent<State>();
+        }
+
+        private void OnMouseDown()
+        {
+            stateObject.SetActive(true);
+            if (_state == null)
+            {
+                Debug.Log("Null");
+            }
+        
+            Debug.Log("Clicked State test");
+            
+            // wind.gameObject.SetActive(true);
+        }
+        
+    }
+}
