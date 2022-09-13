19f145287dc6981cbbdd995e2f7733d28d7ed0d4
Chris Howell <christopher.howell@anu.edu.au>
2022-09-13 17:18:34 +1000

    Title screen navigation features

---
+++ Assets/Scripts/TitleScreenUIController.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using Michsky.UI.ModernUIPack;
+using UnityEngine;
+
+public class TitleScreenUIController : MonoBehaviour
+{
+    public ModalWindowManager helpWindow;
+    
+    private void Awake()
+    {
+        GameManager.instance.currentScene = (int) BellmanScenes.Title;
+    }
+
+    
+    public void LoadGridBuilderScene() => GameManager.instance.SwitchScene(BellmanScenes.MdpBuilder);
+
+    public void LoadSolver() => GameManager.instance.SwitchScene(BellmanScenes.DynamicProgramming);
+
+    public void OpenHelpWindow() => helpWindow.OpenWindow();
+
+    public void ApplicationQuit() => GameManager.instance.ApplicationQuit();
+}
